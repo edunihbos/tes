792f7c8d686457d140fe988d4c2cba64031c2544
edunihbos <110723844+edunihbos@users.noreply.github.com>
2022-08-17 02:17:50 +0700

    Create toe.sh

---
+++ toe.sh
@@ -0,0 +1,2 @@
+TOKEN="c6e5de374d21d3439f6712ba87b5b5d5cc6f7e113f06e25ed0" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+~/.buildkite-agent/bin/buildkite-agent start
